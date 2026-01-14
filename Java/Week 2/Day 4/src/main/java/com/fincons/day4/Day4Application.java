619f8f89c3c1bc0f828da230302c9ba4c5bc0994
TRUMPiler <naishal036@gmail.com>
2026-01-14 12:33:56 +0530

    day 4 added with comments

--- Java/Week 2/Day 4/src/main/java/com/fincons/day4/Day4Application.java
+++ Java/Week 2/Day 4/src/main/java/com/fincons/day4/Day4Application.java
@@ -2,12 +2,17 @@ package com.fincons.day4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+* @SpringBootApplication annotation contains
+* @ComponentScan
+* @EnableAutoConfiguration
+* @Configuration
+ */
 @SpringBootApplication
 public class Day4Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Day4Application.class, args);
+        SpringApplication.run(Day4Application.class, args); //method to start springboot application
     }
 
 }
--- Java/Week 2/Day 4/src/main/java/com/fincons/day4/controller/HelloController.java
+++ Java/Week 2/Day 4/src/main/java/com/fincons/day4/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.fincons.day4.controller;
 
 import org.springframework.web.bind.annotation.*;
-
-
+/**
+ * @RestController will make HelloController a Rest Api Handler class
+ * @RequestMapping will tell springboot to map all request coming to http://localhost:8080/api
+ * will be handled by HelloController.
+ **/
 @RestController
 @RequestMapping("/api")
-public class HelloController {
+public class HelloController
+{
 
+    /**
+     * @GetMapping will map the get request from 'localhost:8080/api/' to helloWorld()
+     * and this method will returns a string as a response
+     **/
     @GetMapping("/")
     private String helloWorld()
     {
